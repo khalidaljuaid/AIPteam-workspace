8a3fe8567524d810e3e4941a92598345443711be
Khalid <khali.@MacBook-Air-Khalid.local>
2025-11-23 23:46:04 +0300

    fix: Remove baked env vars from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    // Make sure DATABASE_URL is available
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
 };
 
 export default nextConfig
